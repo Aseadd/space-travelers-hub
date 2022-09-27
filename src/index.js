feb546b8c05bf36d070e337e95cae822f094530d
robinbenitezmora <robinbenitez1990@gmail.com>
2022-09-27 10:21:48 -0500

    Correct linters errors

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import Rockets from './components/rockets';
 import Profile from './components/profile';
 import Navbar from './components/navbar';
 import './components/navbar.css';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -22,8 +21,3 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
