8cd1bb7a457c732dbdd0a5d32006e4c554edb516
robinbenitezmora <robinbenitez1990@gmail.com>
2022-09-29 08:57:56 -0500

    Correct linters errors version 2

--- src/__tests__/profile.test.js
+++ src/__tests__/profile.test.js
@@ -1,4 +1,4 @@
-import { render, screen} from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import store from '../redux/configureStore';
