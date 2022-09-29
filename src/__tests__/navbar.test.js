676b4fc86f86a4091a856bc2af40bb03bbc68f7c
robinbenitezmora <robinbenitez1990@gmail.com>
2022-09-28 20:23:28 -0500

    Test 'Navbar' component

---
+++ src/__tests__/navbar.test.js
@@ -0,0 +1,19 @@
+import  { screen, render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import store from '../redux/configureStore';
+import Navbar from '../components/navbar';
+
+describe('Navbar', () => {
+  test('renders Navbar component', () => {
+    render(
+      <Provider store={store}>
+        <Router>
+          <Navbar />
+        </Router>
+      </Provider>,
+    );
+    const logo = screen.getByText('Space Travelers\' Hub');
+    expect(logo).toEqual(expect.anything());
+  });
+});
